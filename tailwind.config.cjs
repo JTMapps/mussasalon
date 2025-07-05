2794f67e4e18e28be69ca29f1841d067c14e9b1d
mmaponya08@gmail.com <mmaponya08@gmail.com>
2025-07-05 20:25:18 +0200

    Force add dist for GitHub Pages deployment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 const flowbite = require('flowbite/plugin')
 
 module.exports = {
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
  
 export default defineConfig({
-  base: '/mussasalon/',
-  plugins: [
-    react(),
-    tailwindcss(),   // ★ first-party integration for Tailwind v4+
-  ],
+  plugins: [react()],
+  base: '/', // Vercel = '/', GitHub Pages = '/mussasalon/'
 })
