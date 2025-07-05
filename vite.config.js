40b6871a15394b8dd81c14af06f1b91f54defff6
mmaponya08@gmail.com <mmaponya08@gmail.com>
2025-07-05 16:38:42 +0200

    Redeploy with correct base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+ 
 export default defineConfig({
+  base: '/mussasalon/',
   plugins: [
     react(),
     tailwindcss(),   // ★ first-party integration for Tailwind v4+
